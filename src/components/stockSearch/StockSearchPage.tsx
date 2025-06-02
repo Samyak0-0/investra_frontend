bebdd1605593663e55a6fa959d8c3ea7be9d9c57
Sagar Parajuli <parajuli.sagar759@gmail.com>
2025-06-02 14:51:17 +0545

    stockS not stock

--- src/components/stockSearch/StockSearchPage.tsx
+++ src/components/stockSearch/StockSearchPage.tsx
@@ -78,7 +78,7 @@ const StockSearchPage = () => {
   
       try {
         const response = await fetch(
-          `http://127.0.0.1:5000/api/stock/${ticker}?interval=${interval}`
+          `http://127.0.0.1:5000/api/stocks/${ticker}?interval=${interval}`
         );
         if (!response.ok) {
           throw new Error("Failed to fetch stock data");
@@ -101,7 +101,7 @@ const StockSearchPage = () => {
 
     try {
       const response = await fetch(
-        `http://127.0.0.1:5000/api/stock/${ticker}?interval=${interval}`
+        `http://127.0.0.1:5000/api/stocks/${ticker}?interval=${interval}`
       );
       if (!response.ok) {
         throw new Error("Failed to fetch stock data");
