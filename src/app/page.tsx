2f9222477a751cdf376f021a158d76c5e87dd50f
Lawan-Poudyal <poudyallawan@gmail.com>
2025-06-02 19:40:59 +0545

    changed component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 import StockSearchPage from "@/components/stockSearch/StockSearchPage";
-
+import LoginButton from "@/components/signup/LoginButton";
 
 export default function Home() {
   return (
     <div>
-      <StockSearchPage />
+      <LoginButton />
     </div>
     
   );
