5151bdd21a500ac1b34188e643b6f97e0b10e11a
samyak <samyakmhrzn9841@gmail.com>
2025-08-05 11:37:47 +0545

    forgot to save file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,12 @@ import { useRouter } from "next/navigation";
 import StockDashboard from "@/components/dashboard/Dashboard";
 
 export default function Home() {
-
   const { status } = useSession();
-    const router = useRouter();
-  
-    if (status === "unauthenticated") {
-      router.push("/api/auth/signin")
-    }
+  const router = useRouter();
+
+  if (status === "unauthenticated") {
+    router.push("/api/auth/signin");
+  }
 
   return (
     <div>
